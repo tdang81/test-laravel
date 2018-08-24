2fdae048a3f8b93451c8409541a10bdeecdfd1d3
Trung Dang <dev@dangmedia.de>
2018-08-24 17:43:23 +0200

    init basic auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return 'Login';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
